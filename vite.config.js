509e5e45d2265387852b09229e5db7cc65afb28d
asepependi <asefe019@gmail.com>
2024-06-05 23:00:22 +0700

    first commits

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        laravel([
-            'resources/css/app.css',
-            'resources/js/app.js',
-        ]),
-    ],
-});
+  plugins: [vue()],
+})
